9215a95b5083a45621e9253fd10560699e23f500
sondli <sondre.lillelien@gmail.com>
2025-02-20 14:09:36 +0200

    Added dev cert

--- configuration.nix
+++ configuration.nix
@@ -14,6 +14,8 @@
     };
   };
 
+	programs.thunar.enable = true;
+
   nix.gc = {
     automatic = true;
     dates = "daily";
@@ -113,6 +115,30 @@
   users.extraGroups.vboxusers.members = [ "sondli" ];
 
   security.rtkit.enable = true;
+  security.pki.certificates = [
+    ''
+      -----BEGIN CERTIFICATE-----
+      MIIDDTCCAfWgAwIBAgIJAIK9SIGboH/VMA0GCSqGSIb3DQEBCwUAMBQxEjAQBgNV
+      BAMTCWxvY2FsaG9zdDAeFw0yNDA1MTgxMzU0MDNaFw0yNTA1MTgxMzU0MDNaMBQx
+      EjAQBgNVBAMTCWxvY2FsaG9zdDCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoC
+      ggEBAMnMuV/2VopQRgABBPS3biRtXdGIR27b0AB+nuV7QHkU9N880ouJRe2DbN3Z
+      5oYGMJsIPSQUXUPqvcMQgJsdSkNyGcNQAZVEHAg+oHsD04KctLoT+PDLZxydYl11
+      mBDaRhfEfpl5Pf6xr3Rk7IXNcDzb5ku2DeOu3LBLLZify9yIsL2FzdcC4Nj+ZhgG
+      Dhgwjw541wTmK2Atuu2J4Nuk5j/Y2pRP22BcNHbiPmxrKxI479p8s3pFlDm7EfKN
+      9GaVbM2W97zxX4zvOpn2TRuQN4lY37pz8mx1KFlI0bq3tXOluGiQHdlN0VG+/JSb
+      UxDIcF6hgbD4AneyCpW767xzs0sCAwEAAaNiMGAwDAYDVR0TAQH/BAIwADAOBgNV
+      HQ8BAf8EBAMCBaAwFgYDVR0lAQH/BAwwCgYIKwYBBQUHAwEwFwYDVR0RAQH/BA0w
+      C4IJbG9jYWxob3N0MA8GCisGAQQBgjdUAQEEAQIwDQYJKoZIhvcNAQELBQADggEB
+      ADhf6wYyl5SOALh7CymJRDb//0O88fNu8ZaO765aLepIFgRAJNAADjmpCivqjXRJ
+      L3TwAcCA+axtdlg34cd3lrgKZ0xJ1jcmR3k1kHQk9y3wyW27CHzz/0fVcbcCekj1
+      BLeL00TQ6WO2zW9pZ6XMsqLojtWPBliwK4McBpzUPg+17AQBwtXjODCuKRi2WuFk
+      2AgnhBDdR020E94xR/O58mnXEkmkHVZXzBmJhcAfdO4PCWa+nAQAeOd1hZbZZ2Hn
+      5eIUeiUGXRNv2h/ZAdZ6t2p590UgubLIrPUJgD8wzGCAAYS6rmFmxSOIyEAfH9d6
+      onik1T5+aMTX5J5mOSiWRv8=
+      -----END CERTIFICATE-----
+    ''
+  ];
+
   services.pipewire = {
     enable = true;
     alsa = {
